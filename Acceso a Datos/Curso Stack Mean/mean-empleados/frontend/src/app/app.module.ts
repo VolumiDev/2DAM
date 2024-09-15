7f5b52cb05ca9ae492e0624fd988a806350684fd
VolumiDev <diego.martga@gmail.com>
2024-09-15 19:44:35 +0200

    iniciamos el proyecto de Angular

---
+++ Acceso a Datos/Curso Stack Mean/mean-empleados/frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmpleadoComponent } from './components/empleado/empleado.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmpleadoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
