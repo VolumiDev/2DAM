ddac2cdf0db078aa55453c480dc01ac5628657cb
VolumiDev <diego.martga@gmail.com>
2024-09-23 09:22:12 +0200

    update 240923

--- Sistemas de Gestión Empresarial/Inicio SWING/Ejer_3_16/src/Credential.java
+++ Sistemas de Gestión Empresarial/Inicio SWING/Ejer_3_16/src/Credential.java
@@ -1,7 +1,11 @@
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
@@ -11,19 +15,52 @@ public class Credential extends JFrame{
 
 	private JTextField user, pass;
 	private JLabel lblUser, lblPass;
-	private Button btn;
+	private JButton btn;
 	
 	Credential(){
 		super();
-		 this.setTitle("Incorrecto!!!");
-		 this.setBounds(100, 100, 500, 300);
+		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		this.setTitle("Incorrecto!!!");
+		this.setBounds(100, 100, 300, 200);
 		 
-		 this.setLayout(null);
+		this.setLayout(null);
 
-		 lblUser = new JLabel("User:");
-		 lblUser.setLocation(20, 20);
-		 this.getContentPane().add(lblUser);
+		lblUser = new JLabel("User:");
+		lblUser.setBounds(20, 20, 80, 25);
+		this.getContentPane().add(lblUser);
 		 
-		 this.setVisible(true);
+		user = new JTextField();
+		user.setBounds(110, 20, 150, 25);
+		this.getContentPane().add(user);
+		
+		lblPass = new JLabel("Password:");
+		lblPass.setBounds(20, 60, 80, 25);
+		this.getContentPane().add(lblPass);
+ 		 
+		pass = new JPasswordField();
+		pass.setBounds(110, 60, 150, 25);
+		this.getContentPane().add(pass);
+		
+		btn = new JButton("Login");
+		btn.setBounds(100, 100, 100, 25);
+		btn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(user.getText().equalsIgnoreCase("Juan") && pass.getText().equalsIgnoreCase("abc123")) {
+					JButton boton =(JButton) e.getSource();
+					boton.setText("Correcto");
+				}else {
+					JButton boton =(JButton) e.getSource();
+					boton.setText("Incorrecto!!");
+				}
+			}
+		});
+		this.getContentPane().add(btn);
+		
+		this.setVisible(true);
 	}
 }
--- Sistemas de Gestión Empresarial/Inicio SWING/ExpoControls/src/WCtrl.java
+++ Sistemas de Gestión Empresarial/Inicio SWING/ExpoControls/src/WCtrl.java
@@ -4,6 +4,7 @@ import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class WCtrl extends JFrame{
@@ -11,12 +12,15 @@ public class WCtrl extends JFrame{
 	private JLabel lbl;
 	private JTextField txt, txt1, txt2;
 	private JPasswordField pass;
+	private JTextArea txtArea;
+	
 	
 	
 	WCtrl(){
 		super();
 		
 		this.setTitle("Controls Window");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setBounds(300, 300, 500, 700);
 		this.setLayout(new FlowLayout());
 		this.setResizable(true);
@@ -24,6 +28,7 @@ public class WCtrl extends JFrame{
 		Label();
 		TextField();
 		Pass();
+		textArea();
 		
 		this.setVisible(true);
 	}
@@ -51,5 +56,26 @@ public class WCtrl extends JFrame{
 		pass.setEchoChar('#');
 		
 		this.getContentPane().add(pass);
+	}
+	
+	private void textArea() {
+		txtArea = new JTextArea("Area de muestra/", 10, 10);
+		
+		//corta la palbras cuando llegan al final del area de escritura. NO RESPETA LAS PALABRAS, LAS CORTA
+		txtArea.setLineWrap(true);
+		//Mantiene el formato de las palabras al cortarlas
+		txtArea.setWrapStyleWord(true);
+		//Setea el numero de columnas que tiena el area
+		txtArea.setColumns(10);
+		//cambia el texto entre las posiciones pasadas como parametro
+		txtArea.replaceRange("este es el texto que va a ser cambiado/", 0, 2);
+		//inserta el texto indicado en la posicion indicada
+		txtArea.insert("este texto se a introducido con con la funcion insert/", 0);
+		//añade el texto indicado al final
+		txtArea.append("este txt se ha introducido con el metodo append/");
+		//Determina que numero de lineas contenido en el textarea
+		txtArea.getLineCount();
+	
+		this.getContentPane().add(txtArea);
 	}
 }
