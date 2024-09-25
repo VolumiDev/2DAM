0cc66c5972237876695ef2464093e5d0f7487b42
VolumiDev <diego.martga@gmail.com>
2024-09-25 09:40:29 +0200

    update 25-09-24

--- Interfaces/Tarea3/obj/Debug/net8.0-windows/Tarea3.AssemblyInfo.cs
+++ Interfaces/Tarea3/obj/Debug/net8.0-windows/Tarea3.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Tarea3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db511f9a7f99290577fcc1df3f84f7f58dd4e65e")]
-[assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Tarea3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Tarea3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
