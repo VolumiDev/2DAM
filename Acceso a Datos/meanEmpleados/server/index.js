bbc6e9776296c2a50fddc2123843c1e11a7137ea
unknown <diego.martga@gmail.com>
2024-10-14 19:45:52 +0200

    update

---
+++ Acceso a Datos/meanEmpleados/server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');   //REQUERIMOS EL OBJETO EXPRESS PARA USO
+const app = express();                  
+
+const morgan = require('morgan');     //NOS  DA INFORMACION POR CONSOLA DE LAS PETICIONES GET 
+app.use(morgan('dev'));
+
+
+
+//SETTINGS
+app.set('port', process.env.PORT || 3000);  //ASIGNAMOS A LA VARIABLE PORT EL PUERTO ESTABLECIDO POR SO O POR DEFECTO CON EL 3000
+
+
+//MIDDELWARES
+app.use(express.json());  //LE INDICAMOS QUE ENTIENDA LOS JSON QUE LE LLEGARAN DESDE LA PETICION GET
+
+//ROUTES
+
+app.listen(app.get('port'), () => {                             //HACEMOS QUE LA APP ESCUCHE EN EL EL PUERTO PORT QUE HEMOS ESTABLECIDO ARRIBA.
+    console.log('server ejecutandose en', app.get('port'));
+});
